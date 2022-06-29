28a8797e4bb01e85b08708a35b7d300d38401945
Derek Mason <3derekmason@gmail.com>
2022-06-29 10:51:10 -0600

    add api and header

--- PHP/server.php
+++ PHP/server.php
@@ -1,22 +1,23 @@
 <?php
+  header(API_Key: '7d64ca3869544c469c3e7a586921ba37');
+  $api_endpoint = 'https://api.sightmap.com/v1/assets/1273/multifamily/units';
 
-function getUnits($url){
-  $unitsJson = file_get_contents($url);
-  if ($unitsJson === false) {
-     return false;
+  function getUnits($url){
+    $unitsJson = file_get_contents($url);
+    if ($unitsJson === false) {
+      return false;
+    }
+
+    $unitsObj = json_decode($unitsJson);
+    if ($unitsObj !== null) {
+      return false;
+    }
+
+    return $unitsObj;
   }
 
-  $unitsObj = json_decode($unitsJson);
-  if ($unitsObj !== null) {
-     return false;
+  $units = getUnits($api_endpoint);
+  if ($units !== false) {
+    print $units->family->units;
   }
-
-  return $unitsObj;
-}
-
-$units = getUnits();
-if ($units !== false) {
-  print $units->family->units;
-}
-
 ?>
