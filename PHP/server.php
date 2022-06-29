3d6f226a71690cbb1850db62eab78e05576479b8
Derek Mason <3derekmason@gmail.com>
2022-06-29 16:43:49 -0600

    polish1.0

--- PHP/server.php
+++ PHP/server.php
@@ -1,15 +1,12 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-
   $api_endpoint = 'https://api.sightmap.com/v1/assets/1273/multifamily/units/';
-
   $curl = curl_init($api_endpoint);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($curl, CURLOPT_HTTPHEADER, [
       'API-Key: 7d64ca3869544c469c3e7a586921ba37',
       'Content-type: application/json'
     ]);
-
   $response = curl_exec($curl);
   curl_close($curl);
   echo $response;
