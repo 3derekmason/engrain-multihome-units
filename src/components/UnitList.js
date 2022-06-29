86a1e990aa6992502c5e086de5c7d8d960cd237f
Derek Mason <3derekmason@gmail.com>
2022-06-29 13:26:45 -0600

    toggle unit view based on area

--- src/components/UnitList.js
+++ src/components/UnitList.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./styles/unitList.css";
 
 const UnitList = ({ units }) => {
+  const areaOfOne = units.filter((unit) => unit.area === 1);
+  const areaAboveOne = units.filter((unit) => unit.area > 1);
+  const [currentView, toggleView] = useState(areaOfOne);
+
   return (
     <div className="unitList">
-      {units.map((unit) => (
-        <p>{JSON.stringify(unit)}</p>
-      ))}
+      <div className="filter">
+        <button
+          disabled={currentView === areaOfOne}
+          onClick={() => {
+            toggleView(areaOfOne);
+          }}
+        >
+          Area = 1
+        </button>
+        <button
+          disabled={currentView === areaAboveOne}
+          onClick={() => {
+            toggleView(areaAboveOne);
+          }}
+        >
+          Area > 1
+        </button>
+      </div>
+      <div className="unitContainer">
+        {currentView.map((unit) => (
+          <p>{JSON.stringify(unit)}</p>
+        ))}
+      </div>
     </div>
   );
 };
